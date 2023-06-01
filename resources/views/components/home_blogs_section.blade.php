f0f2f1a71a7ce5cc35805f43ba6cc4663be7bed5
Saw Nay Thar Poe <100279951+sawnaytharpoe02@users.noreply.github.com>
2023-06-01 20:34:49 +0630

    fix responsive breakpoint 768 - 375

--- resources/views/components/home_blogs_section.blade.php
+++ resources/views/components/home_blogs_section.blade.php
@@ -2,12 +2,14 @@
 <section id="home-blogs-section">
   <div class="my-12 mx-7">
     @foreach($blogs as $blog)
-      @if ($loop->index == 5)
-      @break   
-      @else
-        <x-home_blog_card :blog="$blog" :condition="$loop->index % 2 == 0"/>   
-      @endif
+    @if ($loop->index == 5)
+    @break
+    @else
+    <x-home_blog_card :blog="$blog" :condition="$loop->index % 2 == 0" />
+    @endif
     @endforeach
   </div>
-  <div class="flex items-center w-fit mx-auto my-20"><a href="/blogs" class="text-2xl text-steelBlue font-medium">View All Articles</a> <i class="bx bx-chevron-right text-[50px] text-steelBlue"></i></div>
+  <div class="flex items-center w-fit mx-auto my-20"><a href="/blogs"
+      class="text-lg md:text-2xl text-steelBlue font-medium">View All Articles</a> <i
+      class="bx bx-chevron-right text-[50px] text-steelBlue"></i></div>
 </section>
